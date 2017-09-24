fff57db0959463236132f3860e7ce4c4626acae8
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-09-24 20:32:17 +0300

    duplicate files hw

--- peton/dupl.py
+++ peton/dupl.py
@@ -4,28 +4,27 @@ from hashlib import sha1 as hasher
 
 
 def hashing(file_path):
-    with open(file_path, mode='rb') as f:        
-        h = hasher()
-        h.update(f.read())
-        return h.hexdigest()    
+    while True:    
+        with open(file_path, mode='rb') as f:        
+            h = hasher()
+            h.update(f.read(2**15))
+            return h.hexdigest()    
 
 
 def eq_files(catalog):
-    global all_files    
-    for root, dirs, files in os.walk(catalog):
+    for root, _, files in os.walk(catalog):
         for i in files:
             full_path = os.path.join(root,i);
             code = hashing(full_path)            
             if code not in all_files:
-                all_files[code] = [full_path[len(start_path)+1:]]                
+                all_files[code] = [os.path.relpath(full_path, start_path)]                
             else:
                 all_files[code].append(full_path[len(start_path)+1:])
 if __name__ == "__main__":
-#if True:
     all_files = {}
 #    start_path = "/home/ivanmolotkov/Документы/lab02"    
-    start_path = input("enter top_dir ")
+    start_path = sys.argv[1]
     eq_files(start_path)
-    for a in all_files.items():
-        if len(a[1]) > 1:
-            print(":".join(tuple(a[1])))
+    for a in all_files.values():
+        if len(a) > 1:
+            print(":".join(tuple(a)))
