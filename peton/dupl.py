9ec3ffd603a1ed76c13b4203fd65ef52dd8e03ad
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-09-24 19:59:17 +0300

    duplicate files hw

--- peton/dupl.py
+++ peton/dupl.py
@@ -4,31 +4,28 @@ from hashlib import sha1 as hasher
 
 
 def hashing(file_path):
-    h = hasher()
-    f1 = open(file_path, 'rb')
-    h.update(f1.read())
-    return h.hexdigest()    
+    with open(file_path, mode='rb') as f:        
+        h = hasher()
+        h.update(f.read())
+        return h.hexdigest()    
 
 
 def eq_files(catalog):
     global all_files    
-    for _, dirs, files in os.walk(catalog):
-        if (files[0] == "." or files[0] == "~"):
-            continue        
-        full_path = os.path.join(dirs, files)        
-        code = hashing(full_path)            
-        if code not in all_files:
-            all_files[code] = [full_path]                
-        else:
-            all_files[code].append(full_path)
-    
+    for root, dirs, files in os.walk(catalog):
+        for i in files:
+            full_path = os.path.join(root,i);
+            code = hashing(full_path)            
+            if code not in all_files:
+                all_files[code] = [full_path[len(start_path)+1:]]                
+            else:
+                all_files[code].append(full_path[len(start_path)+1:])
 if __name__ == "__main__":
 #if True:
     all_files = {}
+#    start_path = "/home/ivanmolotkov/Документы/lab02"    
     start_path = input("enter top_dir ")
     eq_files(start_path)
     for a in all_files.items():
         if len(a[1]) > 1:
-            for i in range(len(a[1])):
-                print(":".join(a[1]))             
-
+            print(":".join(tuple(a[1])))
