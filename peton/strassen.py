871852dfee70f0bda1d5acf08ec1112591354d6c
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-10-01 18:25:20 +0300

    strassen homework

--- peton/strassen.py
+++ peton/strassen.py
@@ -2,57 +2,54 @@ import math
 import numpy as np
 
 
+def read_array(n):
+    size = 2**(math.ceil(math.log2(n)))
+    a = np.array([[float(i) for i in input().split()] for j in range(n)])
+    return np.vstack((np.hstack((a, np.zeros((n, size-n)))),
+                     np.zeros((size-n, size))))
+
+
+def write_array(arr):
+    for i in range(len(arr)):
+        print(' '.join([str(int(j)) for j in arr[i]]))
+
+
+def quartering(m):
+    size = len(m)//2
+    m11 = m[0:size, 0:size]
+    m12 = m[0:size, size:size*2]
+    m21 = m[size:size*2, 0:size]
+    m22 = m[size:size*2, size:size*2]
+    return m11, m12, m21, m22
+
+
 def strassen(a, b):
-	if len(a) == 1:
-		return a*b
-	
-	size = len(a)//2
+    if len(a) == 1:
+        return a*b
 
-	a11 = a[0:size, 0:size]
-	a12 = a[0:size, size:size*2]
-	a21 = a[size:size*2, 0:size]
-	a22 = a[size:size*2, size:size*2]
+    a11, a12, a21, a22 = quartering(a)
+    b11, b12, b21, b22 = quartering(b)
+    p1 = strassen(a11 + a22, b11 + b22)
+    p2 = strassen(a21 + a22, b11)
+    p3 = strassen(a11, b12 - b22)
+    p4 = strassen(a22, b21 - b11)
+    p5 = strassen(a11 + a12, b22)
+    p6 = strassen(a21 - a11, b11 + b12)
+    p7 = strassen(a12 - a22, b21 + b22)
 
-	b11 = b[0:size, 0:size]
-	b12 = b[0:size, size:size*2]
-	b21 = b[size:size*2, 0:size]
-	b22 = b[size:size*2, size:size*2]
+    c11 = p1 + p4 - p5 + p7
+    c12 = p3 + p5
+    c21 = p2 + p4
+    c22 = p1 - p2 + p3 + p6
 
-	p1 = strassen(a11 + a22, b11 + b22)#(a11 + a22)(b11+b22)
-	p2 = strassen(a21 + a22, b11)#(a21 + a22)(b11)
-	p3 = strassen(a11, b12 - b22)#a11(b12-b22)
-	p4 = strassen(a22, b21 - b11)#a22(b21-b11)
-	p5 = strassen(a11 + a12, b22)#(a11+a12)b22
-	p6 = strassen(a21 - a11, b11 + b12)#(a21-a11)(b11+b12)
-	p7 = strassen(a12 - a22, b21 + b22)#(a12-a22)(b21+b22)
-
-	c11 = p1 + p4 - p5 + p7 #p1 + p4 - p5 + p7
-	c12 = p3 + p5 #p3 + p5
-	c21 = p2 + p4 #p2 + p4
-	c22 = p1 - p2 + p3 + p6#p1 - p2 + p3 + p6
-	c = np.vstack((np.hstack((c11,c12)), np.hstack((c21, c22))))
-	return(c)
+    c = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
+    return c
 
 
-
-
-n = int(input())
-size = 2**(math.ceil(math.log2(n)))
-
-a = np.zeros((size, size))
-b = np.zeros((size, size))
-
-for i in range(n):
-	q = input().split()
-	for j in range(n):
-		a[i][j] = int(q[j]);
-
-for i in range(n):
-	q = input().split()
-	for j in range(n):
-		b[i][j] = int(q[j]);
-
-c = strassen(a, b)
-c = c[0:n, 0:n]
-for i in range(len(c)):
-	print(' '.join([str(int(j)) for j in c[i]]))
+if __name__ = "__main__":
+    n = int(input())
+    a = read_array(n)
+    b = read_array(n)
+    c = strassen(a, b)
+    c = c[0:n, 0:n]
+    write_array(c)
