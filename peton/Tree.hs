f4e8bdc60c094d05353002b57a4f210a1134d67f
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-12-02 14:21:46 +0300

    hw2 haskell

--- peton/Tree.hs
+++ peton/Tree.hs
@@ -1,36 +1,27 @@
+import Prelude hiding (lookup)
+
 data BinaryTree k v = Nil | Node k v (BinaryTree k v) (BinaryTree k v) deriving Show
 
-lookup' x (Node k v l r) | x > k      = lookup' x r
-                         | x < k      = lookup' x l
-                         | otherwise  = Just v
-lookup' _ Nil = Nothing
+lookup x (Node k v l r) | x > k      = lookup x r
+                        | x < k      = lookup x l
+                        | otherwise  = Just v
+lookup _ Nil = Nothing
 
-insert' x y (Node k v l r) | x > k     = Node k v l (insert' x y r)
-                           | x < k     = Node k v (insert' x y l) r
-                           | otherwise = Node x y l r
-insert' x y Nil = Node x y Nil Nil
+insert x y (Node k v l r) | x > k     = Node k v l (insert x y r)
+                          | x < k     = Node k v (insert x y l) r
+                          | otherwise = Node x y l r
+insert x y Nil = Node x y Nil Nil
 
-getKey (Node k _ _ _) = k
-
-getValue (Node _ v _ _) = v
-
-delete' x (Node k v l r) | x > k     = Node k v l (delete' x r)  
-                         | x < k     = Node k v (delete' x l) r
-                         | otherwise = reconstruct' l r
+delete x (Node k v l r) | x > k     = Node k v l (delete x r)  
+                        | x < k     = Node k v (delete x l) r
+                        | otherwise = order l r
                             where
-                                reconstruct' Nil Nil = Nil
-                                reconstruct' l Nil = l
-                                reconstruct' Nil r = r
-                                reconstruct' l r = Node (getKey (lr' r)) (getValue (lr' r)) l (delete' (getKey (lr' r)) r)
+                                order l Nil = l
+                                order Nil r = r
+                                order l r = Node (getKey (mostleft r)) (getValue (mostleft r)) l (delete (getKey (mostleft r)) r)
                                     where
-                                        lr' (Node k v Nil r) = (Node k v Nil r)
-                                        lr' (Node _ _ l _) = lr' l
-delete' _ Nil = Nil
-
-
-
-
-
-
-
---(Node 2 2 (Node 1 3 Nil Nil) (Node 5 4 (Node 4 7 Nil Nil) (Node 8 13 Nil Nil)))
+                                        mostleft (Node k v Nil r) = (Node k v Nil r)
+                                        mostleft (Node _ _ l _) = mostleft l
+                                        getKey (Node k _ _ _) = k
+                                        getValue (Node _ v _ _) = v
+delete _ Nil = Nil
