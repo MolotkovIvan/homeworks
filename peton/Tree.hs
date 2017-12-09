a6baee493c0c6402ea011636a50c7b9401af6156
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-12-09 23:21:25 +0300

    hw 2 Haskell

--- peton/Tree.hs
+++ peton/Tree.hs
@@ -2,14 +2,16 @@ import Prelude hiding (lookup)
 
 data BinaryTree k v = Nil | Node k v (BinaryTree k v) (BinaryTree k v) deriving Show
 
-lookup x (Node k v l r) | x > k      = lookup x r
-                        | x < k      = lookup x l
-                        | otherwise  = Just v
+lookup x (Node k v l r) 
+    | x > k      = lookup x r
+    | x < k      = lookup x l
+    | otherwise  = Just v
 lookup _ Nil = Nothing
 
-insert x y (Node k v l r) | x > k     = Node k v l (insert x y r)
-                          | x < k     = Node k v (insert x y l) r
-                          | otherwise = Node x y l r
+insert x y (Node k v l r) 
+    | x > k     = Node k v l (insert x y r)
+    | x < k     = Node k v (insert x y l) r
+    | otherwise = Node x y l r
 insert x y Nil = Node x y Nil Nil
 
 delete x (Node k v l r) 
@@ -19,8 +21,7 @@ delete x (Node k v l r)
     where
         join l Nil = l
         join Nil r = r
-        join l r = Node k' v' l (delete k' r)
-        (k', v') = leftmost r
-        leftmost (Node k v Nil r) = (k, v)
+        join l r = let (k', v') = leftmost r in Node k' v' l (delete k' r)
+        leftmost (Node k v Nil _) = (k, v)
         leftmost (Node _ _ l _) = leftmost l
 delete _ Nil = Nil
