3245dab126fe32f21322743a40c84fd904183c78
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-12-16 21:57:29 +0300

    sql hw

--- peton/SQL_HW/4.sql
+++ peton/SQL_HW/4.sql
@@ -1,6 +1,5 @@
 SELECT Country.Name, COUNT(City.Population) AS NumberOfMetropolises
 FROM Country
-LEFT JOIN City ON City.CountryCode = Country.Code
-WHERE City.Population >= 1e6
+LEFT JOIN City ON City.CountryCode = Country.Code AND City.Population >= 1e6
 GROUP BY Country.Name
 ORDER BY NumberOfMetropolises DESC
