0ecb300d0a3f08915200629a3f71b1f356def9b6
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-12-27 18:37:36 +0300

    threadpool

--- peton/ThreadPool/threadpool.cpp
+++ peton/ThreadPool/threadpool.cpp
@@ -1,68 +1,111 @@
+#include "threadpool.h"
+#include <iostream>
 #include <pthread.h>
 #include <vector>
 #include <queue>
 
-void thpool_init(struct ThreadPool* pool, unsigned threads_nm) {
-	pthread_cond_init(&pool->cond);
-	pthread_mutex_init(&pool->mutex);
-	pthread_mutex_init(&m_end);
+void* invoke(void*);
+void func(void*);
+
+void thpool_init(ThreadPool* pool, unsigned threads_nm) {
+	pthread_mutex_init(&pool->mutex, NULL);
+	pthread_mutex_init(&pool->m_end, NULL);
+	pthread_cond_init(&pool->cond, NULL);
 	pool->active_threads = threads_nm;
 	for (unsigned i = 0; i < threads_nm; i++) {
 		pthread_t id;
 		pool->threads.push_back(id); 
-		pthread_create(&pool->threads[i], NULL, invoke, pool); 
+		pthread_create(&pool->threads[i], NULL, invoke, (void*)pool); 
 	}
+	std::cout << "0\n";
 }
 
-void thpool_submit(struct ThreadPool* pool, struct Task* task) {
-	pthread_mutex_init(&task->m);
+void thpool_submit(ThreadPool* pool, struct Task* task) {
+	pthread_mutex_init(&task->m, NULL);
 	pool->tasks.push(*task);
 	pthread_cond_signal(&pool->cond);
+
 }
 
 void thpool_wait(struct Task* task) {
 	pthread_mutex_lock(&task->m);
-	while (!task->is_completed) {
-		pthread_cond_wait(&task->is_completed, &task->m);
-	}
+	pthread_cond_wait(&task->is_completed, &task->m);
 	pthread_mutex_unlock(&task->m);
+
 }
 
-void thpool_finit(struct ThreadPool* pool) {
-	pthread_mutex_lock(&m_end);
+void thpool_finit(ThreadPool* pool) {
+	pthread_mutex_lock(&pool->m_end);
 
 	pool->end = true;
-	while (pool->active_threads != 0) {};
-	pthread_mutex_unlock(&m_end);
+	while (pool->active_threads != 0) {
+		std::cout << pool->active_threads << "active_threads\n";
+	}
+	pthread_mutex_unlock(&pool->m_end);
 	pthread_mutex_destroy(&pool->m_end);
 	pthread_mutex_destroy(&pool->mutex);
 	pthread_cond_destroy(&pool->cond);
+
 }
 
-void* invoke(struct ThreadPool* pool) {
+void* invoke(void* pool) {
 	while(true) {
-		pthread_mutex_lock(&pool->mutex);
-		while (pool->tasks.empty()) {
-			pthread_mutex_lock(&m_end);
-			if (end) { 
-				pool->active_threads -= 1;
+		std::cout << "1\n";
+		ThreadPool* p = (ThreadPool*)pool;
+		pthread_mutex_lock(&p->mutex);
+		std::cout << "2\n";
+		while (p->tasks.empty()) {
+			std::cout << "6\n";
+			pthread_mutex_lock(&p->m_end);
+			std::cout << "7\n";
+			if (p->end) { 
+				p->active_threads -= 1;
 				return NULL;
 			}
-			pthread_mutex_unlock(&m_end);
-			pthread_cond_wait(&pool->cond, &pool->mutex);
+			std::cout << "8\n";
+			pthread_mutex_unlock(&p->m_end);
+			std::cout << "9\n";
+			pthread_cond_wait(&p->cond, &p->mutex);
 		}
-		if (!pool->tasks.empty()) {
-			Task t = pool->tasks.pop();
-			pthread_mutex_lock(&t->m);
-			t.is_completed = true;
-			void f = t.f;
+		std::cout << p->tasks.size() <<" 3\n";
+		if (!p->tasks.empty()) {
+			pthread_mutex_lock(&p->tasks.front().m);
+			Task t = p->tasks.front();
+			p->tasks.pop();
+			void (*f)(void*);
+			f = t.f;
 			f(t.arg);
-			pthread_mutex_unlock(&t->m);
-			pthread_cond_broadcast(&t->iscompleted);
-			pthread_mutex_destroy(&t->m);
-			pthread_cond_destroy(&t->iscompleted);
+			pthread_mutex_unlock(&t.m);
+			pthread_cond_broadcast(&t.is_completed);
+			pthread_mutex_destroy(&t.m);
+			pthread_cond_destroy(&t.is_completed);
 		}
+		std::cout << "4\n";
 		
-		pthread_mutex_unlock(&pool->mutex);
+		pthread_mutex_unlock(&p->mutex);
+		std::cout << "5\n";
 	}
+}
+
+void func(void* arg) {
+	int* n = (int*)arg;
+	std::cout << *n << "\n";
+}
+
+int main () {
+	ThreadPool pool;
+	thpool_init(&pool, 2);
+	Task *t1 = new Task;
+	t1->f = func;
+	int n1 = 22;
+	t1->arg = &n1;
+	thpool_submit(&pool, t1);
+	Task *t2 = new Task;
+	t2->f = func;
+	int n2 = 6;
+	t2->arg = &n2;
+	
+	thpool_finit(&pool);
+	std::cout << "end\n";
+	return 0;
 }
--- peton/ThreadPool/threadpool.h
+++ peton/ThreadPool/threadpool.h
@@ -1,6 +1,8 @@
 #ifndef THREAD_POOL
 #define THREAD_POOL
-
+#include "pthread.h"
+#include <vector>
+#include <queue>
 struct Task {
     void (*f)(void*);
     void* arg;
@@ -14,12 +16,12 @@ struct ThreadPool {
 	pthread_mutex_t m_end;
     pthread_mutex_t mutex;     
     pthread_cond_t cond;    
-    vector<pthread_t>;
-    queue<Task> tasks;
+    std::vector<pthread_t> threads;
+    std::queue<Task> tasks;
 };
 
-void thpool_init(struct ThreadPool* pool, unsigned threads_nm);
-void thpool_submit(struct ThreadPool* pool, struct Task* task);
-void thpool_wait(struct Task* task);
-void thpool_finit(struct ThreadPool* pool);
+void thpool_init(ThreadPool* pool, unsigned threads_nm);
+void thpool_submit(ThreadPool* pool, struct Task* task);
+void thpool_wait(Task* task);
+void thpool_finit(ThreadPool* pool);
 #endif
