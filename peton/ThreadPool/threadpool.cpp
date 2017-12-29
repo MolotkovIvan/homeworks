a23e9d8d65e11f9d16645a3068a1814a8ac9c88e
Molotkov Ivan <molotkov.ivan.igorevich@gmail.com>
2017-12-29 13:50:54 +0300

    threadpool

--- peton/ThreadPool/threadpool.cpp
+++ peton/ThreadPool/threadpool.cpp
@@ -51,13 +51,11 @@ void thpool_finit(ThreadPool* const pool) {
 	pthread_mutex_unlock(&pool->mutex);
 //	std::cout << "2 finit\n";
 	pthread_cond_broadcast(&pool->new_task);
-	while (pool->active_threads == 0) {
+	for (int i = 0; i < pool->threads.size(); i++) {
 //		std::cout << "3 finit\n";
-		pthread_mutex_lock(&pool->mutex); 
-		pthread_cond_wait(&pool->finit, &pool->mutex);
-		pthread_mutex_unlock(&pool->mutex); 
+		pthread_join(pool->threads[i], NULL);
 //		std::cout << "4 finit\n";
-	}
+	}	
 	
 	pthread_mutex_destroy(&pool->mutex);
 //	std::cout << "5 finit\n";
@@ -66,17 +64,20 @@ void thpool_finit(ThreadPool* const pool) {
 void* invoke(void* const pool) {
 	ThreadPool* p = (ThreadPool*)pool;
 	while(true) {
+//		std::cout << "1 invoke\n";
 		pthread_mutex_lock(&p->mutex);
 		while (p->tasks.empty() || p->end) {
-			pthread_cond_wait(&p->new_task, &p->mutex);
+//			std::cout << "2 invoke\n";
 			if (p->end) {
+//				std::cout << "3 invoke\n";
 				--p->active_threads;
-				pthread_cond_signal(&p->finit);
+				//pthread_cond_signal(&p->finit);
+				pthread_mutex_unlock(&p->mutex);
 				return NULL;
 			}
-
+			pthread_cond_wait(&p->new_task, &p->mutex);
 		};
-
+//		std::cout << "4 invoke\n";
 		if (!p->tasks.empty() && !p->end) {
 			Task *t = p->tasks.front();
 			pthread_mutex_lock(&t->m);
@@ -90,31 +91,112 @@ void* invoke(void* const pool) {
 	}
 }
 
-void func(void* arg) {
-	int* n = (int*)arg;
-	std::cout << *n << "\n";
+struct input {
+	int l;
+	int m;
+	int r;
+	int* array;
+};
+
+void sort (void* arg) {
+	input* data = (input*)arg;
+	int tmp;
+	for (int i = data->l; i < data->r; i++) {
+		for (int j = data->l; j < data->r + data->l - i - 1; j++) {		
+			if (data->array[j] > data->array[j+1]) {
+				tmp = data->array[j];
+				data->array[j] = data->array[j+1];
+				data->array[j+1] = tmp;
+			}
+		}
+	}
+}
+
+void merge (void* arg) {
+	input* data = (input*)arg;
+	int i = data->l;
+	int j = data->m;
+	int* buf = new int[data->r - data->l];
+	int k = 0;
+	while (i < data->m && j < data->r) {
+		if (data->array[i] < data->array[j]) {
+			buf[k] = data->array[i];
+			i++;
+		} else {
+			buf[k] = data->array[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < data->m) {
+		buf[k] = data->array[i];
+		i++;
+		k++;
+	}
+	while (j < data->r) {
+		buf[k] = data->array[j];
+		j++;
+		k++;
+	}
+	for (int q = data->l; q < data->r; q++) {
+		data->array[q] = buf[q-data->l];
+	}
+
+	delete [] buf;
 }
 
 int main () {
 	ThreadPool pool;
 	thpool_init(&pool, 2);
 	
+
+	int* a = new int[4];
+	a[0] = 4;
+	a[1] = 7;
+	a[2] = 8;
+	a[3] = 1;
+
 	Task *t1 = new Task;
-	t1->f = func;
-	int n1 = 22;
-	t1->arg = &n1;
+	t1->f = sort;
 	t1->iscompleted_bool = false;
-	thpool_submit(&pool, t1);
-	thpool_wait(t1);
+	input *arg1 = new input;
+	arg1->l = 0;
+	arg1->r = 2;
+	arg1->array = a;
+	t1->arg = arg1;
 	
 	Task *t2 = new Task;
-	t2->f = func;
-	int n2 = 6;
-	t2->arg = &n2;
+	t2->f = sort;
 	t2->iscompleted_bool = false;
+	input *arg2 = new input;
+	arg2->l = 2;
+	arg2->r = 4;
+	arg2->array = a;
+	t2->arg = arg2;
+	
+	Task *t3 = new Task;
+	t3->f = merge;
+	t3->iscompleted_bool = false;
+	input *arg3 = new input;
+	arg3->l = 0;
+	arg3->m = 2;
+	arg3->r = 4;
+	arg3->array = a;
+	t3->arg = arg3;
+
+	thpool_submit(&pool, t1);
+	thpool_wait(t1);
+
 	thpool_submit(&pool, t2);
 	thpool_wait(t2);
-	
+
+	thpool_submit(&pool, t3);
+	thpool_wait(t3);
+
 	thpool_finit(&pool);
+	for (int i = 0; i < 4; i++) {
+		std::cout << a[i] << " ";
+	}
+	std::cout << "\n";
 	return 0;
 }
